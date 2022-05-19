c3074a58c0e9ffc0fd540e1e0712fa4a9fc510af
JeremyNjonge <jeremynjonge@gmail.com>
2022-05-19 11:38:50 +0300

    Update and rename cryptocurrency_prices to cryptocurrency_prices.py

--- cryptocurrency_prices.py
+++ cryptocurrency_prices.py
@@ -7,5 +7,5 @@ start = dt.date(2021,1,1) # Start date
 end = dt.datetime.now() # End date
 crypto = ['ETH-USD'] 
 
-df = reader.get_data_yahoo(crypto,start,end) # Dataframe with ETH-USD data
-df
+df = reader.get_data_yahoo(crypto,start,end)['Close'] # Dataframe with ETH-USD data
+df.to_csv('ethdata.csv') # Save to csv file
